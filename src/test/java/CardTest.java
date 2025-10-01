502967956a18d7a60b693b8d4910e5127f486a5e
AlexGreasley <agreasley5@gmail.com>
2025-10-01 18:40:06 +0100

    Added JUnit tests for Card.java

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class CardTest {
+    @Test
+    public void constructorTest_validNumber() {
+        Card card = new Card(5);
+        assertEquals(Integer.valueOf(5), card.getNumber());
+    }
+
+    @Test
+    public void constructorTest_nullNumber() {
+        new Card(null);
+    }
+
+    @Test
+    public void constructorTest_invalidNumber() {
+        new Card(-1);
+    }
+}
