c10da9d7cfd280fadc294c24ec47ccc9dd4822fb
AlexGreasley <agreasley5@gmail.com>
2025-10-01 20:39:36 +0100

    Added CreatePack.java creating 10 testing files.

---
+++ src/test/java/main/CreatePack.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class CreatePack {
+    public static void main(String[] args) {
+        int numPlayers = 10;
+        createPack(numPlayers);
+    }
+
+    public static void createPack(int numPlayers) {
+        int totalCards = numPlayers * 8;
+
+        List<Integer> pack = new ArrayList<Integer>();
+
+        for (int num = 1; num < numPlayers + 1; num++) {
+            for (int i = 0; i < 4; i++) {
+                pack.add(num);
+            }
+        }
+
+        Random rand = new Random();
+        int remaining = totalCards - pack.size();
+        for (int i = 0; i < remaining; i++) {
+            int randomNum = rand.nextInt(numPlayers) + (numPlayers + 1);
+            pack.add(randomNum);
+        }
+
+        Collections.shuffle(pack);
+
+        String folderPath = "src/main/resources/packs/";
+        File folder = new File(folderPath);
+        String fileName = "pack" + numPlayers + ".txt";
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (int num : pack) {
+                writer.write(num + "\n");
+            }
+            System.out.println("Pack created successfully!");
+        } catch (IOException e) {
+            System.err.println("Error writing the pack file: " + e.getMessage());
+        }
+    }
+}
